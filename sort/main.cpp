8e39eb5c056ec510532538a0c5402e83658b90a2
bigthumb <achoco@163.com>
2015-04-09 22:18:50 +0800

    Add comments

--- sort/main.cpp
+++ sort/main.cpp
@@ -12,15 +12,18 @@
 
 int main(int argc, const char* argv[])
 {
-    int a[] = {13, 9, 2, 12, 3, 16, 10};
-    int alen = sizeof(a) / sizeof(int);
+    {
+        // øÏÀŸ≈≈–Ú
+        int a[] = {13, 9, 2, 12, 3, 16, 10};
+        int alen = sizeof(a) / sizeof(int);
 
-    myqsort(a, 0, alen - 1);
+        myqsort(a, 0, alen - 1);
 
-    int b[] = {2, 4, 5, 5, 10, 16, 20};
-    int blen = sizeof(a) / sizeof(int);
+        int b[] = {2, 4, 5, 5, 10, 16, 20};
+        int blen = sizeof(a) / sizeof(int);
 
-    myqsort(b, 0, blen - 1);
+        myqsort(b, 0, blen - 1);
+    }
 
     return 0;
 }
